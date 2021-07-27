e372fd2cec8fb43b4bcf04f13786bc94351b4f21
juliocnsouzadev <juliocnsouzadev@gmail.com>
2021-07-27 07:23:59 -0300

    dockerize

--- src/test/java/info/juliocnsouza/gcpstackdriverlogging/GcpStackdriverLoggingApplicationTests.java
+++ src/test/java/info/juliocnsouza/gcpstackdriverlogging/GcpStackdriverLoggingApplicationTests.java
@@ -1,4 +1,4 @@
-package com.anoophp.gcpstackdriverlogging;
+package info.juliocnsouza.gcpstackdriverlogging;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class GcpStackdriverLoggingApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
